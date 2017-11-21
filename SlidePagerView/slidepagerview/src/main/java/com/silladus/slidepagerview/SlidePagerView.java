1a8c6779d8b8da1724de8bb0246dfe2c43c0eace
silladus <silladus@163.com>
2017-11-21 09:17:45 +0800

    修正缩放系数的计算
    1.修正缩放系数的计算
    2.去除nineoldandroids的依赖

--- SlidePagerView/slidepagerview/src/main/java/com/silladus/slidepagerview/SlidePagerView.java
+++ SlidePagerView/slidepagerview/src/main/java/com/silladus/slidepagerview/SlidePagerView.java
@@ -13,9 +13,6 @@ import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 
-import com.nineoldandroids.animation.FloatEvaluator;
-import com.nineoldandroids.view.ViewHelper;
-
 /**
  * Created by silladus on 2017/11/16/0016.
  * GitHub: https://github.com/silladus
@@ -27,10 +24,6 @@ public class SlidePagerView extends LinearLayout implements GestureDetector.OnGe
      * 手势监听器
      */
     private GestureDetector mGestureDetector;
-    /**
-     * 变化计算
-     */
-    private FloatEvaluator mFloatEvaluator;
     /**
      * 当前选中界面的宽度
      */
@@ -51,6 +44,14 @@ public class SlidePagerView extends LinearLayout implements GestureDetector.OnGe
      * 滑动变化量
      */
     private float mScrollX = selectWidth + distance;
+    /**
+     * 缩放动画持续时间
+     */
+    private long scaleAnimDuration = 600;
+
+    public void setScaleAnimDuration(long scaleAnimDuration) {
+        this.scaleAnimDuration = scaleAnimDuration;
+    }
 
     public SlidePagerView(@NonNull Context context) {
         this(context, null);
@@ -66,8 +67,6 @@ public class SlidePagerView extends LinearLayout implements GestureDetector.OnGe
     }
 
     private void init(Context context) {
-        mFloatEvaluator = new FloatEvaluator();
-
         setClickable(true);
         mGestureDetector = new GestureDetector(context, this);
         setOnTouchListener(new OnTouchListener() {
@@ -88,8 +87,7 @@ public class SlidePagerView extends LinearLayout implements GestureDetector.OnGe
 
         // 初始化大小
         for (int i = 1; i < getChildCount(); i++) {
-            ViewHelper.setScaleX(getChildAt(i), mFloatEvaluator.evaluate(1, 1, 0.9));//x方向上缩小
-            ViewHelper.setScaleY(getChildAt(i), mFloatEvaluator.evaluate(1, 1, 0.9));//y方向上缩小
+            scale(getChildAt(i), evaluate(1, 1, 0.9));// 缩小
         }
         selectWidth = getChildAt(0).getMeasuredWidth();
     }
@@ -161,24 +159,19 @@ public class SlidePagerView extends LinearLayout implements GestureDetector.OnGe
             public void onAnimationUpdate(ValueAnimator animation) {
                 mScrollX = (float) animation.getAnimatedValue();
                 scrollTo((int) (mScrollX * direction + index * scrollX), 0);
-                float fraction = Math.abs(scrollX - mScrollX * direction) / scrollX;
+                // 计算动画速率
+                // float fraction = (scrollX - Math.abs(mScrollX * direction)) / scrollX;
+                float fraction = 1 - Math.abs(mScrollX * direction) / scrollX;
                 if (index == 0) {
-                    ViewHelper.setScaleX(getChildAt(index), mFloatEvaluator.evaluate(fraction, 0.9, 1));//x方向上放大
-                    ViewHelper.setScaleY(getChildAt(index), mFloatEvaluator.evaluate(fraction, 0.9, 1));//y方向上放大
-                    ViewHelper.setScaleX(getChildAt(index + 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//x方向上缩小
-                    ViewHelper.setScaleY(getChildAt(index + 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//y方向上缩小
+                    scale(getChildAt(index), evaluate(fraction, 0.9, 1));// 放大
+                    scale(getChildAt(index + 1), evaluate(fraction, 1, 0.9));// 缩小
                 } else if (index == getChildCount() - 1) {
-                    ViewHelper.setScaleX(getChildAt(index), mFloatEvaluator.evaluate(fraction, 0.9, 1));//x方向上放大
-                    ViewHelper.setScaleY(getChildAt(index), mFloatEvaluator.evaluate(fraction, 0.9, 1));//y方向上放大
-                    ViewHelper.setScaleX(getChildAt(index - 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//x方向上缩小
-                    ViewHelper.setScaleY(getChildAt(index - 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//y方向上缩小
+                    scale(getChildAt(index), evaluate(fraction, 0.9, 1));// 放大
+                    scale(getChildAt(index - 1), evaluate(fraction, 1, 0.9));// 缩小
                 } else {
-                    ViewHelper.setScaleX(getChildAt(index), mFloatEvaluator.evaluate(fraction, 0.9, 1));//x方向上放大
-                    ViewHelper.setScaleY(getChildAt(index), mFloatEvaluator.evaluate(fraction, 0.9, 1));//y方向上放大
-                    ViewHelper.setScaleX(getChildAt(index - 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//x方向上缩小
-                    ViewHelper.setScaleY(getChildAt(index - 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//y方向上缩小
-                    ViewHelper.setScaleX(getChildAt(index + 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//x方向上缩小
-                    ViewHelper.setScaleY(getChildAt(index + 1), mFloatEvaluator.evaluate(fraction, 1, 0.9));//y方向上缩小
+                    scale(getChildAt(index), evaluate(fraction, 0.9, 1));// 放大
+                    scale(getChildAt(index - 1), evaluate(fraction, 1, 0.9));// 缩小
+                    scale(getChildAt(index + 1), evaluate(fraction, 1, 0.9));// 缩小
                 }
             }
         });
@@ -193,9 +186,31 @@ public class SlidePagerView extends LinearLayout implements GestureDetector.OnGe
             }
         });
 
-        mFlingAnimator.setDuration(1020);
+        mFlingAnimator.setDuration(scaleAnimDuration);
         mFlingAnimator.setInterpolator(new DecelerateInterpolator());
         mFlingAnimator.start();
+    }
+
+    /**
+     * view缩放
+     */
+    protected void scale(View view, float scaleValue) {
+        view.setScaleX(scaleValue);// X方向上缩放
+        view.setScaleY(scaleValue);// Y方向上缩放
+    }
+
+    /**
+     * 变动后值的实时计算
+     *
+     * @param fraction 百分占比
+     * @param startValue 变动起始值
+     * @param endValue 变动终值
+     * @return 变动后自身的值
+     */
+    public Float evaluate(float fraction, Number startValue, Number endValue) {
+        float startFloat = startValue.floatValue();
+        //noinspection UnnecessaryBoxing
+        return Float.valueOf(startFloat + fraction * (endValue.floatValue() - startFloat));
     }
 
     public interface OnSelectListener {
